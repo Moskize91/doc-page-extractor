9410a71ee16c2c0eeaf83d7be98c32bdb5d6c265
TaoZeyu <i@taozeyu.com>
2025-04-11 18:11:03 +0800

    test(project): to test table in windows

--- table.py
+++ table.py
@@ -3,14 +3,18 @@ from doc_page_extractor.table import TableParsingStructEqTable
 
 def main():
   table = TableParsingStructEqTable({
-    "model_path": "/Users/taozeyu/codes/github.com/moskize91/doc-page-extractor/model/table",
+    # "model_path": "models/TabRec/StructEqTable",
     "max_new_tokens": 1024,
     "max_time": 30,
     "output_format": "latex",
     "lmdeploy": False,
     "flash_attn": True,
   })
-  table.predict(["/Users/taozeyu/codes/github.com/moskize91/doc-page-extractor/tests/images/table_item.png"])
+  result = table.predict([
+    "C:\\Users\\i\\codes\\github.com\\moskize91\\doc-page-extractor\\tests\\images\\table_item.png",
+  ])
+  print("\nResult:")
+  print(result[0])
 
 if __name__ == "__main__":
   main()
