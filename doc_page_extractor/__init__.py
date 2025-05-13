256254d5d9446ddb0ef6caa5d3c9dfa363d938e7
l1shen <lishen1635@gmail.com>
2025-05-13 14:36:35 +0800

    fix export

--- doc_page_extractor/__init__.py
+++ doc_page_extractor/__init__.py
@@ -3,7 +3,6 @@ from .clipper import clip, clip_from_image
 from .plot import plot
 from .rectangle import Point, Rectangle
 from .types import (
-  ExtractParams,
   ExtractedResult,
   OCRFragment,
   LayoutClass,
