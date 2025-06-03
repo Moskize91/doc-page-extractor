f5d4412d236385bde50f042c86bb31cd973d6daf
TaoZeyu <i@taozeyu.com>
2025-06-03 16:21:28 +0800

    feat(project): should export Model & HuggingfaceModel

--- doc_page_extractor/__init__.py
+++ doc_page_extractor/__init__.py
@@ -2,6 +2,7 @@ from .extractor import DocExtractor
 from .clipper import clip, clip_from_image
 from .plot import plot
 from .rectangle import Point, Rectangle
+from .model import Model, HuggingfaceModel
 from .types import (
   ExtractedResult,
   OCRFragment,
